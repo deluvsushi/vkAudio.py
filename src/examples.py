5ed3a41d4de80d7cf6af38d9ef7a06a42c1f6628
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-04-05 20:52:19 +0600

    Update examples.py

--- src/examples.py
+++ src/examples.py
@@ -1,4 +1,4 @@
-import vkAudio
-VkAudioClient = vkaudio.VkAudioClient(access_token="")
+import vk_audio
+VkAudioClient = vk_audio.VkAudioClient(access_token="")
 popular_audios = VkAudioClient.get_popular_audios(count=100)
 print(popular_audios)
